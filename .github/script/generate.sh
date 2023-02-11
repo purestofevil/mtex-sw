adee3c1aa5e74a1ed875fdedcff55ecd63be9da1
purestofevil <32451545+purestofevil@users.noreply.github.com>
2023-02-11 12:23:08 +0100

    textgeneration: initial working version

--- .github/script/generate.sh
+++ .github/script/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for type in st la be ent muster schulung; do 
+for type in st be ent; do 
 	for json in json/${type}/*.json; do 
 		ruby .github/script/chain.rb ${json} > texte/$type/$(echo $json | sed -e 's/^.*\///' -e 's/.json//').md 
 	done; 
